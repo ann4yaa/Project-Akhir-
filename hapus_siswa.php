b7b910401138705eaa07c0c31790d3bf904747ea
Hagia Sofiana <hagiasofiana@gmail.com>
2026-05-28 17:08:01 +0700

    has new files

---
+++ hapus_siswa.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include "koneksi.php";
+$id = $_GET['id_siswa'];
+
+$cek = mysqli_query($conn, "SELECT * FROM siswa WHERE id='$id'");
+$data = mysqli_fetch_assoc($cek);
+
+if(!$data){
+    header("location: siswa.php?p=Data tidak ditemukan!");
+    exit();
+}
+
+$hapus = mysqli_query($conn, "DELETE FROM siswa WHERE id = '$id'");
+
+if ($hapus) {
+    header("location: siswa.php?p=Data berhasil dihapus!");
+    exit();
+} else {
+    header("location: siswa.php?p=Data gagal dihapus!");
+    exit();
+}
+?>
