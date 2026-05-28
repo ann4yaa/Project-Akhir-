5a60daf010f5887ea0435e4ec8db509d0dba07b5
Hagia Sofiana <hagiasofiana@gmail.com>
2026-05-28 17:01:07 +0700

    has add new files

--- koneksi.php
+++ koneksi.php
@@ -1,18 +1,20 @@
 <?php
 
-$hostname = "Localhost";
+$hostname = "localhost";
 $username = "root";
 $password = "";
-$database = "hagiaRajendra_penilaian";
+$database = "hagiaRajendra_penilaian"; 
 
 $conn = mysqli_connect($hostname, $username, $password);
 
-if (!$conn) {
+if ($conn) {
+
     $pilih_db = mysqli_select_db($conn, $database);
-    if($pilih_db){
-        //
-    } else {
-        echo "Koneksi Gagal, di periksa lagi.";
+    
+    if (!$pilih_db) {
+        echo "Database tidak ditemukan!";
     }
+} else {
+    echo "Koneksi ke server MySQL Gagal.";
 }
 ?>
